cb235c4bc0c0f08b5fc28097ad93a3346175caac
Penttala Janne O <jannepen@cubbli18-gold.local>
2023-05-25 15:51:21 +0300

    eslint fix
    Co-authored-by: susannakinnunen <susanna.m.kinnunen@helsinki.fi>

--- server/tests/login_api.test.js
+++ server/tests/login_api.test.js
@@ -1,49 +1,49 @@
 const supertest = require('supertest')
 const bcrypt = require('bcrypt')
-const {User, sequelize} = require('../models/user')
+const {User} = require('../models/user')
 const app = require('../app')
 
 const api = supertest(app)
 
 beforeEach(async () => {
-  User.destroy({
-    where: {},
-    truncate: true
-  })
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash('salainen', saltRounds)
+    User.destroy({
+        where: {},
+        truncate: true
+    })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash('salainen', saltRounds)
 
-  const initialUser = [
-    {
-      name: 'Pekka Testinen',
-      username: 'Pekka35',
-      password: passwordHash,
-    }
-  ]
-  await User.create(initialUser[0])
+    const initialUser = [
+        {
+            name: 'Pekka Testinen',
+            username: 'Pekka35',
+            password: passwordHash,
+        }
+    ]
+    await User.create(initialUser[0])
 })
 
 test('existing user can log in', async () => {
-  const existingUser = {
-    username: 'Pekka35',
-    password: 'salainen'
-  }
+    const existingUser = {
+        username: 'Pekka35',
+        password: 'salainen'
+    }
 
-  await api
-    .post('/api/login')
-    .send(existingUser)
-    .expect(200)
-  })
+    await api
+        .post('/api/login')
+        .send(existingUser)
+        .expect(200)
+})
 
 test('non-existing user can not log in', async () => {
-  const existingUser = {
-    username: 'Jaakko35',
-    password: 'salainen2'
-  }
+    const existingUser = {
+        username: 'Jaakko35',
+        password: 'salainen2'
+    }
 
-  await api
-  .post('/api/login')
-  .send(existingUser)
-  .expect(401)
+    await api
+        .post('/api/login')
+        .send(existingUser)
+        .expect(401)
 })
 
