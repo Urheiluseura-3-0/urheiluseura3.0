18eaa029c6b71d2fcdb4668ad464587ee5794425
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-05-25 12:49:42 +0300

    Update user model

--- server/models/user.js
+++ server/models/user.js
@@ -5,22 +5,118 @@ const sequelize = new Sequelize(process.env.DATABASE_URL)
 class User extends Model {}
 
 User.init({
-    id : {
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    name: {
-        type: DataTypes.TEXT,
-        allowNull:false
-    },
     username: {
         type: DataTypes.TEXT,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: [5,15]
+        }
     },
     password: {
         type: DataTypes.TEXT,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: [5,50]
+        }
+    },
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            len: [5,50]
+        }
+    },
+    lastname: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [5,50]
+        }
+    },
+    identityCode: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [11]
+        }
+    },
+    address: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [5,50]
+        }
+    },
+    zipcode: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [5],
+            isInt: true
+        }
+    },
+    city: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [5,50]
+        }
+    },
+    phonenumber: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            len: [5,50]
+        }
+    },
+    email: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+            isEmail: true,
+            len: [5,50]
+        }
+    },
+    adminApproved: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        validate: {
+            min: 0,
+            max: 1
+        }
+    },
+    isAdmin: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0,
+            max: 1
+        }
+    },
+    isWorker: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        validate: {
+            min: 0,
+            max: 1
+        }
+    },
+    isForeman: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0,
+            max: 1
+        }
     }
 }, {
     sequelize,
