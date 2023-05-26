f78a6ed75f3c91c60029283767972d5473519edc
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-05-26 14:07:14 +0300

    Update register POST with new fields

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -6,15 +6,37 @@ const User = require('../models/user')
 
 registerRouter.post('/', async (request, response) => {
     try {
-        const { name, newusername, newpassword } = request.body
+        const name = request.body.name
+        const lastName = request.body.lastName
+        const username = request.body.newusername
+        const password = request.body.newpassword
+        const address = request.body.address
+        const city = request.body.city
+        const postalCode = request.body.postalCode
+        const phoneNumber = request.body.phoneNumber
+        const email = request.body.email
+        const identityCode = request.body.identityCode
+        
+        const finduser = await User.findOne({where: {username: username}})
+        
+        if (finduser) {
+            return response.status(401).json({error: 'Käyttäjätunnus on jo olemassa.'}) 
+        }
 
         const saltRounds = 10
-        const passwordHash = await bcrypt.hash(newpassword, saltRounds)
+        const passwordHash = await bcrypt.hash(password, saltRounds)
 
         const user = new User({
-            name: name,
-            username: newusername,
+            username: username,
             password: passwordHash,
+            name: name,
+            lastName: lastName,
+            address: address,
+            city: city,
+            postalCode: postalCode,
+            phoneNumber: phoneNumber,
+            email: email,
+            identityCode: identityCode
         })
 
         const savedUser = await User.create(user.dataValues)
@@ -24,5 +46,7 @@ registerRouter.post('/', async (request, response) => {
         return response.status(400)
     }
 })
+
+
 
 module.exports = registerRouter
