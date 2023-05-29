aa64219291423d44bb4f9478fe0071f77cc4d493
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-05-29 11:37:44 +0300

    Validate register input

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -2,6 +2,46 @@ const bcrypt = require('bcrypt')
 const registerRouter = require('express').Router()
 const {User} = require('../models/user')
 
+const validateNotEmpty = (fields) => {
+    let isOk = true
+    fields.forEach(field => {
+        if (field === '' || field === undefined) {
+            isOk=false
+        }
+    })
+
+    return isOk
+}
+
+const validateLength = (fields, min, max) => {
+    let isOk = true
+    fields.forEach(field => {
+        if (field !== undefined && field.length > 0) {
+            if (field.length < min || field.length > max) {
+                isOk = false
+            }
+        }
+    })
+
+    return isOk
+}
+
+const validateRegisterInput = (name, lastName, username, password, address, city,
+    postalCode, phoneNumber, email, identityCode) => {
+    if (!validateNotEmpty([name, username, password])) {
+        return false
+    } else if (!validateLength([username], 5, 15)) {
+        return false
+    } else if (!validateLength([postalCode], 5, 5)) {
+        return false
+    } else if (!validateLength([identityCode], 11, 11)) {
+        return false
+    } else if (!validateLength([name, lastName, address, city, phoneNumber, email], 0, 50)) {
+        return false
+    } else {
+        return true
+    }
+}
 
 registerRouter.post('/', async (request, response) => {
     try {
@@ -22,6 +62,11 @@ registerRouter.post('/', async (request, response) => {
             return response.status(401).json({error: 'Käyttäjätunnus on jo olemassa.'}) 
         }
 
+        if (!validateRegisterInput(name, lastName, username, password, address, city, 
+            postalCode, phoneNumber, email, identityCode)) {
+            return response.status(401).json({error: 'Virheellinen syöte.'})
+        }
+
         const saltRounds = 10
         const passwordHash = await bcrypt.hash(password, saltRounds)
 
@@ -40,7 +85,7 @@ registerRouter.post('/', async (request, response) => {
 
         const savedUser = await User.create(user.dataValues)
 
-        response.json(savedUser)
+        return response.status(200).json(savedUser)
     } catch (error) {
         return response.status(400)
     }
