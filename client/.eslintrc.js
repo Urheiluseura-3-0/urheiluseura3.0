68efe51d1205c906bdceb67adced21833bf2f255
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-05-30 11:31:19 +0300

    fixed eslint

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         'browser': true,
         'es6': true,
         'jest/globals': true,
-        'cypress/globals': true
+        'cypress/globals': true,
     },
     'extends': [
         'eslint:recommended',
@@ -52,5 +52,5 @@ module.exports = {
         'react': {
             'version': 'detect'
         }
-    }
+    },
 }
