b8a944336d37a47b007324d687b80bc11b041a10
juaxE <juho.kirvesmaki@helsinki.fi>
2023-06-01 15:20:49 +0300

    Sequelize refactored and added new models

---
+++ server/models/event.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/db')
+
+class Event extends Model {}
+
+Event.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    opponent:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            len: [2, 40]
+        }
+    },
+    location:{
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            len: [2, 40]
+        }
+    },
+    dateTime:{
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+            len: [2, 40]
+        }
+    },
+    description:{
+        type: DataTypes.TEXT,
+        validate: {
+            len: [0, 200]
+        }
+    },
+    status:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: 0,
+            max: 1
+        }
+    }
+}, {
+    sequelize,
+    underscored: true,
+    modelName: 'event'
+})
+
+module.exports = Event
