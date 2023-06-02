f9d0f416749ce06411e23287c2fcc48ccedfb969
Penttala Janne O <jannepen@cubbli18-gold.local>
2023-06-02 14:47:56 +0300

    More event tests

--- client/cypress/e2e/event.cy.js
+++ client/cypress/e2e/event.cy.js
@@ -20,10 +20,107 @@ describe('Event', function() {
         cy.get('#password').type('salainen1234')
         cy.get('#login-button').click()
     })
+
     describe('Page view', function() {
+
         it('user can see event form', function() {
             cy.url().should('include','/home')
             cy.contains('Lisää tapahtuma')
         })
+
+//        it('user cannot send form if fields are empty',function() {
+//            cy.get('#add-event').should('be.disabled')
+//        })
     })
+
+    describe('Team check', function() {
+        beforeEach(function() {
+            cy.get('input[id=opponent]').type('honka')
+            cy.get('input[id=location]').type('espoon halli')
+            cy.get('input[id=date]').type('2023-06-01')
+            cy.get('input[id=time]').type('11:00')
+            cy.get('input[id=description]').type('tuomarointi')
+        })
+
+//        it('user cannot send form without inputing team', function() {
+//            cy.get('#add-event').should('be.disabled')
+//        })
+    })
+
+    describe('Opponent check', function() {
+        beforeEach(function() {
+            cy.get('select[id=team]').select('Joukkue 1')
+            cy.get('input[id=location]').type('espoon halli')
+            cy.get('input[id=date]').type('2023-06-01')
+            cy.get('input[id=time]').type('11:00')
+            cy.get('input[id=description]').type('tuomarointi')
+        })
+
+//        it('user cannot send form if opponent name is too short', function() {
+//            cy.get('input[id=opponent]').type('A')
+//            cy.get('Body').click({ x:266, y:32 })
+//            cy.get('#add-event').should('be.disabled')
+//        })
+
+//        it ('opponent name will not be too long', function() {
+//            cy.get('input[id="opponent"]').type('Chargoggagoggmanchauggagoggchaubunagungamaugg')
+//            cy.get('input[id="opponent"]').invoke('val').should('have.length', 40)
+//        })
+    })
+
+    describe('Location check', function() {
+        beforeEach(function() {
+            cy.get('select[id=team]').select('Joukkue 1')
+            cy.get('input[id=opponent]').type('honka')
+            cy.get('input[id=date]').type('2023-06-01')
+            cy.get('input[id=time]').type('11:00')
+            cy.get('input[id=description]').type('tuomarointi')
+        })
+
+//        it('user cannot send form if location name is too short', function() {
+//            cy.get('input[id=location]').type('E')
+//            cy.get('Body').click({ x:266, y:32 })
+//            cy.get('#add-event').should('be.disabled')
+//        })
+
+//        it ('location name will not be too long', function() {
+//            cy.get('input[id="location"]').type('Chargoggagoggmanchauggagoggchaubunagungamaugg')
+//            cy.get('input[id="location"]').invoke('val').should('have.length', 40)
+//        })
+    })
+
+    describe('Date check', function() {
+        beforeEach(function() {
+            cy.get('select[id=team]').select('Joukkue 1')
+            cy.get('input[id=opponent]').type('honka')
+            cy.get('input[id=location]').type('espoon halli')
+            cy.get('input[id=time]').type('11:00')
+            cy.get('input[id=description]').type('tuomarointi')
+        })
+
+//        it('user cannot send form without inputing date', function() {
+//            cy.get('#add-event').should('be.disabled')
+//        })
+    })
+
+    describe('Time check', function() {
+        beforeEach(function() {
+            cy.get('select[id=team]').select('Joukkue 1')
+            cy.get('input[id=opponent]').type('honka')
+            cy.get('input[id=location]').type('espoon halli')
+            cy.get('input[id=date]').type('2023-06-01')
+            cy.get('input[id=description]').type('tuomarointi')
+        })
+
+//        it('user cannot send form without inputing time', function() {
+//            cy.get('#add-event').should('be.disabled')
+//        })
+    })
+
+//    describe('Description check', function() {
+//        it('description will not be too long', function() {
+//            cy.get('input[id="description"]').type('Espoo Basket Team (EBT) on Suomen Koripalloliiton jäsenseura. EBT on Espoon Akilleksen ja EPS-Basketin fuusiona vuonna 1993 perustettu koripallon erikoisseura.Seura järjestää lasten, nuorten ja aikuisten koripallotoimintaa, niin harraste-, kilpa- kuin huipputasolla.')
+//            cy.get('input[id="description"]').invoke('val').should('have.length', 200)
+//        })
+//    })
 })
