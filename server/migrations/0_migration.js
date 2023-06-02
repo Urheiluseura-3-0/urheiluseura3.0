fd67fc4d2a5c93b239a8e7e94e1034b22d94af0c
juaxE <juho.kirvesmaki@helsinki.fi>
2023-06-02 13:45:39 +0300

    first version for migration file

---
+++ server/migrations/0_migration.js
@@ -0,0 +1,136 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+    up: async({ context: queryInterface}) => {
+        await queryInterface.createTable('users', {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            username: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    len: [5,15]
+                }
+            },
+            password: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+            },
+            firstName: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [2, 40]
+                }
+            },
+            lastName: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [2, 40]
+                }
+            },
+            address: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [2, 40]
+                }
+            },
+            postalCode: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [5, 5]
+                }
+            },
+            city: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [2, 40]
+                }
+            },
+            phoneNumber: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [5, 15]
+                }
+            },
+            email: {
+                type: DataTypes.TEXT,
+                allowNull: false,
+                validate: {
+                    len: [5, 40]
+                }
+            },
+            personNumber: {
+                type: DataTypes.INTEGER,
+                allowNull:false,
+                defaultValue: -1
+            },
+            adminApproved: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 1,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            },
+            isAdmin: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            },
+            isCoach: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 1,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            },
+            isWorker: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 1,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            },
+            isSupervisor: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            },
+            isForeman: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+                validate: {
+                    min: 0,
+                    max: 1
+                }
+            }
+        })
+    },
+    down: async ({context: queryInterface}) => {
+        await queryInterface.dropTable('users')
+    }
+}
