d4a96391b123b837efb44d88cf2c22bd84be581b
juaxE <juho.kirvesmaki@helsinki.fi>
2023-06-05 13:19:58 +0300

    capitalized references in model index

--- server/models/index.js
+++ server/models/index.js
@@ -4,16 +4,16 @@ const Event = require('./event')
 const Supervisor = require('./supervisor')
 
 
-User.hasMany(Event, { foreignKey: 'createdByUserId', as: 'createdEvents' })
-User.hasMany(Event, { foreignKey: 'confirmedByUserId', as: 'confirmedEvents' })
+User.hasMany(Event, { foreignKey: 'createdByUserId', as: 'CreatedEvents' })
+User.hasMany(Event, { foreignKey: 'confirmedByUserId', as: 'ConfirmedEvents' })
 User.belongsToMany(Team, { through: 'Supervisor', foreignKey: 'userId' })
 
-Team.hasMany(Event, { foreignKey: 'teamId', as: 'events' })
+Team.hasMany(Event, { foreignKey: 'teamId', as: 'TeamEvents' })
 Team.belongsToMany(User, { through: 'Supervisor', foreignKey: 'teamId' })
 
-Event.belongsTo(User, { foreignKey: 'createdByUserId', as: 'createdBy' })
-Event.belongsTo(Team, { foreignKey: 'teamId', as: 'team' })
-Event.belongsTo(User, { foreignKey: 'confirmedBySupervisorId', as: 'confirmedBy' })
+Event.belongsTo(User, { foreignKey: 'createdByUserId', as: 'CreatedBy' })
+Event.belongsTo(Team, { foreignKey: 'teamId', as: 'EventTeam' })
+Event.belongsTo(User, { foreignKey: 'confirmedBySupervisorId', as: 'ConfirmedBy' })
 
 Supervisor.belongsTo(User)
 Supervisor.belongsTo(Team)
