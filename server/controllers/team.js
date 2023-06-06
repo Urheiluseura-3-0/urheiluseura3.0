a03b7607ae11733b55875a243d853a4d26c7111e
juaxE <juho.kirvesmaki@helsinki.fi>
2023-06-06 16:14:50 +0300

    post team first version

--- server/controllers/team.js
+++ server/controllers/team.js
@@ -26,4 +26,25 @@ teamRouter.get('/:id', async (request,response) => {
         return response.status(400).end()
     }
 })
+teamRouter.post('/', async (request, response) => {
+    try {
+        const {name, category } = request.body
+
+        const finduser = await Team.findOne({where: {name: name}})
+        if (finduser) {
+            return response.status(401).json({error: 'Joukkue on jo olemassa'})
+        }
+        const team = new Team({
+            name: name,
+            category: category
+        })
+
+        const savedTeam = await Team.create(team.dataValues)
+
+        return response.status(200).json(savedTeam)
+    } catch (error) {
+        return response.status(400)
+    }
+})
+
 module.exports =  teamRouter
