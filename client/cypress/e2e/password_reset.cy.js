5bd477da94142680066012cef54b73850158dffb
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-06-15 15:28:02 +0300

    password reset updated frontend tests

--- client/cypress/e2e/password_reset.cy.js
+++ client/cypress/e2e/password_reset.cy.js
@@ -17,69 +17,65 @@ describe('Password reset', function() {
         cy.request('POST', 'http://localhost:3001/api/register/', user)
 
 
-        cy.visit('http://localhost:3000/resetpassword')
+        cy.visit('http://localhost:3000/resetpassword/invaliditoken')
     })
 
 
     describe('Page view', function() {
         it('user can see reset form', function() {
             cy.url().should('include', '/resetpassword')
-            cy.contains('Vaihda salasana')
+            cy.contains('Vaihda salasana') })
 
-
-            /* if send-button is disabled then check
-            it('user cannot send form if fields are empty', function() {
+        it('user cannot send form if fields are empty', function() {
             cy.get('#send').should('be.disabled')
-        }) */
-
         })
-    })
 
+    })
 
     describe('Password reset with correct input and token', function() {
         /* beforeEach(function() {
-            cy.visit('http://localhost:3000/resetpassword:token')
+            cy.visit('http://localhost:3000/resetpassword:validToken')
         })*/
+
         it('user sees a notification/is directed to right place after succesful password reset', function() {
             cy.get('input[id="password"]').type('salainen12345')
             cy.get('input[id="passwordConfirmed"]').type('salainen12345')
             cy.get('#send').click()
-            // Onnistuneen salasanan lähetyksen jälkeiset tapahtumat
+            /* Onnistuneen salasanan lähetyksen jälkeiset tapahtumat
+            cy.contains('Salasanan vaihto onnistui)
         })
 
         it('user can login in with the reset password', function() {
-            cy.visit('http://localhost:3000')
+            cy.get('input[id="password"]').type('salainen12345')
+            cy.get('input[id="passwordConfirmed"]').type('salainen12345')
+            cy.get('#send').click()
+
+            cy.get('#front-page-link').click()
+
             cy.get('#username').type('Tiina14')
-            cy.get('#password').type('salainen1234')
+            cy.get('#password').type('salainen12345')
             cy.get('#login-button').click()
 
-            // mitä näkyy kirjautuneelle
+            cy.contains('Tapahtumat') */
         })
     })
 
 
-    describe('Password reset with incorrect input or missing token', function() {
+    describe('Password reset with incorrect input or invalid token', function() {
         it('user sees a notification if password is too short', function() {
-            cy.get('input[id= "password"]').type('sala')
-            //cy.get('#send').should('be.disabled')
-            //notifikaatio tsekkaus
-        })
-
-        it('user sees a notification if password is too long', function() {
-            cy.get('input[id= "password"]').type('12345_12345_12345_12345_12345_12345_12345')
-            //cy.get('#send').should('be.disabled')
-            //notifikaatio tsekkaus
+            cy.get('input[id="password"]').type('sala')
+            cy.contains('Salasanan minimipituus on 10 merkkiä')
+            cy.get('#send').should('be.disabled')
         })
 
         it('user sees a notification if given passwords do not match', function() {
             cy.get('input[id="password"]').type('salainen12345')
             cy.get('input[id="passwordConfirmed"]').type('Salainen12345')
-            cy.get('#send').click()
-
-            // cy.contains('Salasanat eivät täsmää')
+            cy.contains('Salasanat eivät täsmää tai se on liian lyhyt')
+            cy.get('#send').should('be.disabled')
         })
 
-        it('user sees a notification if resetting without a token', function() {
+        it('user sees a notification if resetting with invalid token', function() {
             cy.get('input[id="password"]').type('salainen12345')
             cy.get('input[id="passwordConfirmed"]').type('salainen12345')
             cy.get('#send').click()
