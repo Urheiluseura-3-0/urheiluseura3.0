27a568f674c680298bf9e48908214ca45655eb44
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-06-15 11:45:45 +0300

    password reset form tests second version

--- client/cypress/e2e/password_reset.cy.js
+++ client/cypress/e2e/password_reset.cy.js
@@ -24,15 +24,41 @@ describe('Password reset', function() {
         cy.get('#username').type('Tiina14')
         cy.get('#password').type('salainen1234')
         cy.get('#login-button').click() })
-})
 
-describe('Page view', function() {
-    it('user can see reset form', function() {
-        cy.url().should('include', 'MUUTA')
-        cy.contains('MUUTA')
+
+    describe('Page view', function() {
+        it('user can see reset form', function() {
+            cy.url().should('include', '/resetpassword')
+            cy.contains('Vaihda salasana')
+
+        })
+        /* if send-button is disabled then check
+            it('user cannot send form if fields are empty', function() {
+            cy.get('# MUUTA').should('be.disabled')
+        })*/
     })
-    /* if send-button is disabled then check
-    it('user cannot send form if fields are empty', function() {
-        cy.get('# MUUTA').should('be.disabled')
-    })*/
+
+    describe('Password reset with correct input', function() {
+        it('user sees a notification/is directed to right place after succesful password reset ', function() {
+            cy.get('input[id="password"').type('salainen12345')
+            cy.get('input[id="passwordConfirmed"').type('salainen12345')
+            cy.get('#send').click()
+        })
+
+        // Onnistuneen salasanan lähetyksen jälkeiset tapahtumat
+    })
+
+    describe('Password reset with incorrect input', function() {
+        it('user sees a notification if password is too short', function() {
+            cy.get('input[id= "password"]').type('sala')
+        })
+
+        it('user sees a notification if given passwords do not match', function() {
+            cy.get('input[id= "password" ]').type('salainen12345')
+            cy.get('input[id= " passwordConfimed"]').type('Salainen12345')
+            cy.get('#send').click()
+        })
+    })
+
+
 })
