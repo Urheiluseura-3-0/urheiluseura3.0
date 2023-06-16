d3d2adc65246b5f01b1164eda66bc7f058c13003
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-06-16 10:52:32 +0300

    Move send reset email to own module

---
+++ server/controllers/email.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer')
+
+const sendResetEmail = async (email, token) => {
+    const account = await nodemailer.createTestAccount()
+    
+    const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    },
+    {
+        from: 'Lähettäjä <lahettaja@jokuosoite.com>'
+    })
+
+    const message = {
+        to: email,
+        subject: 'Salasanalinkki',
+        text: `http://localhost:3000/resetpassword/${token}`,
+        html: `<a href="http://localhost:3000/resetpassword/${token}">Vaihda salasana</a>`
+    }
+
+
+    transporter.sendMail(message, (error, info) => {
+        if (error) {
+            console.log('Viestiä ei lähetetty', error)
+            return false
+        }
+
+        console.log('Viesti lähetetty', nodemailer.getTestMessageUrl(info))
+    })
+
+    return true
+}
+
+module.exports = { sendResetEmail }
