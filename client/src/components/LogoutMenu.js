c01f29142164c6d1b93c64ff159e0725b32e5cdc
Jannepen <janne_96_@hotmail.com>
2023-06-16 13:45:59 +0300

    Tailwind styling

--- client/src/components/LogoutMenu.js
+++ client/src/components/LogoutMenu.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 const LogoutMenu = () => {
     return(
         <div>
-            <div id='navigationbar' className='flex justify-center space-x-4'>
-                <Link id='login-link' className= 'rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/">Kirjaudu sisään</Link>
-                <Link id='register-link' className= 'rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }}  to="/register">Rekisteröidy</Link>
+            <div id='navigationbar' className='flex justify-center space-x-4 h-10 bg-cyan-300'>
+                <Link id='login-link' className= 'flex items-center bg-white rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/">Kirjaudu sisään</Link>
+                <Link id='register-link' className= 'flex items-center bg-white rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }}  to="/register">Rekisteröidy</Link>
             </div>
         </div>
     )
--- client/src/components/UserMenu.js
+++ client/src/components/UserMenu.js
@@ -6,11 +6,11 @@ const UserMenu = ({ handleLogout }) => {
     return(
 
         <div>
-            <div id='navigationbar' className='flex justify-center space-x-4'>
-                <Link id='frontpage-link' className= 'rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/home">Etusivu</Link>
-                <Link id='addevent-link'className= 'rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/event">Lisää tapahtuma</Link>
-                <Link id='addjob-link'className= 'rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/job">Lisää työtunnit</Link>
-                <button id='logout-button' className= 'bg-teal-400 hover:bg-teal-600 px-5 py-1 leading-5 rounded-full font-semibold text-white' style= {{ padding:5, border: '1px solid black', position: 'absolute', top:0, right:0 }} onClick= {handleLogout}>Kirjaudu ulos</button>
+            <div id='navigationbar' className='flex justify-center space-x-4 h-10 bg-cyan-300'>
+                <Link id='frontpage-link' className= 'flex items-center bg-white rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/home">Etusivu</Link>
+                <Link id='addevent-link'className= 'flex items-center bg-white rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/event">Lisää tapahtuma</Link>
+                <Link id='addjob-link'className= 'flex items-center bg-white rounded ring-1 ring-gray-700 ring-opacity-50 hover:ring-gray-700 hover:bg-teal-200 text-sm font-semibold text-gray-600 text-center' style={{ padding:5 }} to="/job">Lisää työtunnit</Link>
+                <button id='logout-button' className= 'h-10 bg-red-400 hover:bg-red-500 px-5 py-1 leading-5 rounded-full font-semibold text-white' style= {{ padding:5, border: '1px solid black', position: 'absolute', top:0, right:0 }} onClick= {handleLogout}>Kirjaudu ulos</button>
             </div>
         </div>)
 
